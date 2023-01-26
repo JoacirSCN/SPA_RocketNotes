b3261c7230653cc7aad7d463bc4608f87a77a74f
joacir <joacirprog@gmail.com>
2023-01-26 12:11:52 -0300

    SignUp finished

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import GlobalStyle from "./styles/global";
 
 import theme from "./styles/theme";
 
+// Rotas
 import { Details } from "./pages/Details";
 import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
